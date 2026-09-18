ea26ad4d81e1491a9b6065a00a2d0d401e85c44e
naveedaziz2312 <naveedaziz2312@gmail.com>
2026-09-19 02:47:24 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-self.addEventListener('install',e=>{self.skipWaiting();});
-self.addEventListener('activate',e=>{self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+const CACHE='bali-v2';const FILES=['./','./index.html','./script.js','./manifest.json','./icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
